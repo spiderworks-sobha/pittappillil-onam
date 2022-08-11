0f819632e4175b074520ec6159e380114c03c969
ArunSpiderWorks <arun@spiderworks.in>
2022-08-11 16:54:00 +0530

    latest changes

---
+++ storage/framework/views/9406c4e338efe9dead81a2c39ae96b94bd6998c6.php
@@ -0,0 +1,3 @@
+<div class="invoice-response w-100 text-center">
+    <p class="font-weight-bold text-success">You have won a <span><?php echo e($gift->gift->name); ?></span></p>
+</div><?php /**PATH D:\Xampp\htdocs\pittappillil-onam\resources\views/web/invoice_result.blade.php ENDPATH**/ ?>
