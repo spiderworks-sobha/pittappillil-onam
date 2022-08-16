ad35d6268d8e0c89ff3892e6a142735c6bbb61d4
ArunSpiderWorks <arun@spiderworks.in>
2022-08-16 17:36:52 +0530

    latest changes

--- resources/views/web/thankyou.blade.php
+++ resources/views/web/thankyou.blade.php
@@ -38,24 +38,10 @@
                             <div class="form-cntr-main text-center ">
                                 <h3 class="green-text">Congratulations!</h3>
                                 <div class="presents">
-                                    <div class="present orange">
-                                    <div class="lid"></div>
-                                    <div class="box"></div>
-                                    <div class="bow"></div>
-                                    <div class="ribbon"></div>
+                                    <div class="giftbox-container">
+                                   <img src="{{asset('public/assets/web')}}/img/gift-box.png" alt="..."> 
                                     </div>
-                                    <div class="present blue">
-                                    <div class="lid"></div>
-                                    <div class="box"></div>
-                                    <div class="bow"></div>
-                                    <div class="ribbon"></div>
-                                    </div>
-                                    <div class="present green">
-                                    <div class="lid"></div>
-                                    <div class="box"></div>
-                                    <div class="bow"></div>
-                                    <div class="ribbon"> </div>
-                                    </div>
+                                 
                                 </div>
                                 <p>You have received a <span><b>{{$gift->name}}</b></span></p>
                             </div>
